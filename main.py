1d2703dd2ed0e98b537c41728240babcbba8926b
Payal Patel <payalpn@live.unc.edu>
2016-04-26 00:10:17 +0000

    Payal's Project Update 3

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
-fileOptions = """Welcome! Select a file to get started! 
-A - organization one 
-B - organization two
-C - organization three
-D - Enter a new file"""
-
-options = """1 - Main Menu
-2 - Option 2
-3 - Option 3
-4 - Option 4"""
-
+from options import * 
+ 
 def fileSelection():
 #include try/except here? if one of the file options is not chosen? 
   print(fileOptions)
   file = input("Select a file:")
-  if file == "A" or "a":
-    print("You are now using organization one's membership data file")
+  
+  if file == "A" or file == "a":
+    print("organization ONE")
     fname = "organization1.csv"
-  elif file == "B" or "b":
-    print("You are now using organization two's membership data file")
+  elif file == "B" or file == "b":
+    print("organization TWO")
     fname = "organization2.csv" 
-  elif file == "C" or "c":
-    print("You are now using organization three's membership data file")
+  elif file == "C" or file == "c":
+    print("organization THREE")
     fname = "organization3.csv"
-  elif file == "D" or "d": 
+  elif file == "D" or file == "d": 
     print("user file")
     fname = input("Enter file name:")
   else:
     print("Please select a file")
+  
   global fhand
   fhand = open(fname) 
   global selectedFile
   selectedFile = fhand.readlines()
-  
-fileSelection()
-
-#stores column headings from file into list, columnHeadings --used laterin option 4 
-columnHeadings = []
-columnHeadings = selectedFile[0]
-columnHeadings = columnHeadings.split(",")
-# print(columnHeadings) 
-
-
-def option2():
-  print("You selected option 2")
+ 
+    
+def option4():
+  print("You selected option 4")
   companyCounts = dict()
   for line in selectedFile:
     # print(line)
@@ -92,22 +77,128 @@ def option3():
       print("\t".join(b))
 
 
-def option4():
-  print("You selected option 4")
+def option2():
+  print("You selected option 2")
   UserNameInput = input("Enter username:")
   for line in selectedFile: 
     line = line.split(",")
     username = line[1]
     if UserNameInput == username: 
       starter = 0 
+      newLine = {}
       for i in line:
-        newLine = {columnHeadings[starter], i}
-        print(newLine)
+        newLine[columnHeadings[starter]] = i
         starter += 1 
+        
+      newLine['salary'] = newLine['salary'].rstrip()
+      
+      newLineList = list(newLine.keys())
+      newLineList.sort()
+      for key in columnHeadings:
+          d = list()
+          d = (key, str(newLine[key]))
+          # print(d)
+          print(": ".join(d))  
+      # print(newLine)
     else:
       continue
-  
+
+
+def option5():
+  total = 0
+  totalFemale = 0
+  totalMale = 0 
+  count = 0 
+  female = 0
+  male = 0 
+  jobCount = 0 
+  jobSalary = 0
+  print(avgOptions)
+  x = input("Enter something:")
+  if x =="a":
+    for line in selectedFile:
+      line = line.split(",")
+      if line[13] == "salary\n": 
+        continue
+      else:
+        salary = line[13]
+        # print(salary)
+        salary = salary.rstrip()
+        # print(salary)
+        salary = int(salary)
+        total = salary + total 
+        count = count + 1
+    
+    average = total / count 
+    print("Average Salary:","$",average)
+  elif x == "b":
+    for line in selectedFile:
+      line = line.split(",")
+      if line[13] == "salary\n": 
+        continue
+      elif line[5] == "Female":
+        salary = line[13]
+        salary = salary.rstrip()
+        salary = int(salary)
+        totalFemale = salary + totalFemale 
+        female = female + 1
+      elif line[5] == "Male":
+        salary = line[13]
+        salary = salary.rstrip()
+        salary = int(salary)
+        totalMale = salary + totalMale 
+        male = male + 1
+    averageFemale = round(totalFemale/female, 2)
+    averageMale = round(totalMale/male, 2) 
+    print("Female Average Salary:","$",averageFemale)
+    print("Male Average Salary:", "$",averageMale)
+  elif x == "c":
+    job = input("Enter job title:")
+    for line in selectedFile:
+      line = line.split(",")
+      if line[13] == "salary\n": 
+        continue
+      elif job == line[9]: 
+        salary = line[13]
+        salary = salary.rstrip()
+        salary = int(salary)
+        jobSalary = jobSalary + salary 
+        jobCount = jobCount + 1
+    avgJobSalary = round(jobSalary/jobCount, 2)
+    print(job, "Average Salary:","$",avgJobSalary)
+
+def option6():
+  print("you selected option 6")
+  states = dict()
+  for line in selectedFile:
+    line = line.split(",")
+    state = line[11]
+    if state not in states:
+      states[state] = 1
+    else: 
+      states[state] += 1 
+  # print(states)
+  statesList = list(states.keys())
+  statesList.sort()
+  for key in statesList:
+    if key == "state":
+      continue
+    else:
+      a = states[key]
+      print(key, a * '*', "(", a, ")")
+    
+
+fileSelection() 
+ 
 going = True
+#stores column headings from file into list, columnHeadings --used laterin option 4 
+columnHeadings = []
+columnHeadings = selectedFile[0]
+columnHeadings = columnHeadings.split(",")
+columnHeadings[13] = columnHeadings[13].rstrip()
+# print(columnHeadings) 
+
+#general demograhics option--count by gender, race, etc? 
 
 while going:
   print(options)
@@ -120,6 +211,10 @@ while going:
     option3()
   elif choice == "4":
     option4() 
+  elif choice == "5":
+    option5()
+  elif choice == "6":
+    option6()
   elif choice == "exit":
     going = False
     
---
+++ options.py
@@ -0,0 +1,16 @@
+fileOptions = """Welcome! Select a file to get started! 
+A - Organization One Data File 
+B - Organization Two Data File
+C - Organization Three Data File
+D - Enter a New Data File"""
+
+options = """1 - Main Menu
+2 - Option 2-search by username
+3 - Option 3
+4 - Option 4
+5 - Option 5
+6 - Option 6"""
+
+avgOptions = """a - Main Menu
+b - Avg. Salary by Gender
+c - Avg. Salary by Position"""
